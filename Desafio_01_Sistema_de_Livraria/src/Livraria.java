5fd873c8518e51968ecdf08c9015834beef3673f
Cesar Augusto <augustocesar875@gmail.com>
2025-05-08 23:12:11 -0300

    add menu

--- Desafio_01_Sistema_de_Livraria/src/Livraria.java
+++ Desafio_01_Sistema_de_Livraria/src/Livraria.java
@@ -2,55 +2,64 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Livraria {
+    private static Biblioteca biblioteca = new Biblioteca();
+    private static Scanner visual = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Biblioteca biblioteca = new Biblioteca();
-        Scanner visual = new Scanner(System.in);
+        Scanner valor = new Scanner(System.in);
         System.out.println("**Bem-vindo à Livraria ☕ Java da Rocketset**");
 
         while (true) {
-            System.out.println("Deseja ver os livros disponíveis ? (y/n)");
-            String querVer = visual.next();
+            System.out.println("Escolha uma opção:");
+            System.out.println("1 - Deseja ver os livros disponíveis ?");
+            System.out.println("2 - Deseja fazer o cadastro de algum livro?");
+            System.out.println("3 - Deseja pegar algum livro emprestado?");
+            System.out.println("4 - Finalizar.");
+            int escolha = valor.nextInt();
 
-            if (querVer.equalsIgnoreCase("y")) {
-                visual.nextLine();
-                System.out.println("Livros disponíveis:");
-                biblioteca.listarLivros();
-
-                System.out.println("Deseja fazer o cadastro de algum livro? (y/n)");
-                String querCadastro = visual.next();
-
-                if (querCadastro.equalsIgnoreCase("y")) {
-                    visual.nextLine(); // limpar o buffer
-                    System.out.println("Informe o nome do livro");
-                    String titulo = visual.nextLine();
-                    System.out.println("Informe o nome do autor:");
-                    String autor = visual.nextLine();
-                    biblioteca.adicionarLivro(titulo, autor);
-                    System.out.println("Livro cadastrado");
-                    System.out.println("Livros disponiveis Atualizados: ");
-                    biblioteca.listarLivros();
-                }
-
-                System.out.println("Deseja pegar algum livro emprestado? (y/n)");
-                String querPegarLivro = visual.next();
-
-                if (querPegarLivro.equalsIgnoreCase("y")) {
-                    System.out.println("Informe o ID do livro solicitado:");
-                    int idLivro = visual.nextInt();
-                    Livro emprestado=biblioteca.emprestarLivroPorId(idLivro);
-                    if (emprestado !=null){
-                        System.out.println("Livro emprestado com sucesso: " + emprestado.getTitulo());
-                    } else {
-                        System.out.println("❌ Livro não disponível ou ID inválido.");
-                    }
-                }
-
-            } else {
-                System.out.println("Obrigado por procurar a livraria ☕ Java da Rocketseat, volte sempre!");
-                break;
+            switch (escolha) {
+                case 1:
+                    livrosDisponiveis();
+                    break;
+                case 2:
+                    CadastroLivro();
+                    break;
+                case 3:
+                    livroEmprestado();
+                    break;
+                case 4:
+                    System.out.println("Obrigado por procurar a livraria ☕ Java da Rocketseat, volte sempre!");
+                    return;
+                default:
+                    System.out.println("Opção inválida!");
             }
         }
     }
+
+    public static void livrosDisponiveis() {
+        System.out.println("Livros disponíveis:");
+        biblioteca.listarLivros();
+    }
+
+    public static void CadastroLivro() {
+        System.out.println("Informe o nome do livro");
+        String titulo = visual.nextLine();
+        System.out.println("Informe o nome do autor:");
+        String autor = visual.nextLine();
+        biblioteca.adicionarLivro(titulo, autor);
+        System.out.println("Livro cadastrado");
+        System.out.println("Livros disponiveis Atualizados: ");
+        biblioteca.listarLivros();
+    }
+
+    public static void livroEmprestado() {
+        System.out.println("Informe o ID do livro solicitado:");
+        int idLivro = visual.nextInt();
+        Livro emprestado = biblioteca.emprestarLivroPorId(idLivro);
+        if (emprestado != null) {
+            System.out.println("Livro emprestado com sucesso: " + emprestado.getTitulo());
+        } else {
+            System.out.println("❌ Livro não disponível ou ID inválido.");
+        }
+    }
 }
-
-
